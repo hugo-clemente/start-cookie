dc84ce2710e892df696b85ec7c95cbe06ff9583a
Hugo Clemente <hugo.clemente@tradon.fr>
2026-05-17 18:07:31 +0200

    chore: bootstrap start-cookie package

---
+++ tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  platform: 'neutral',
+  dts: true,
+  exports: {
+    packageJson: true,
+    legacy: true,
+  },
+  clean: true,
+})
